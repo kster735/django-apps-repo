181077525b96246413b2931da466837146fdc19e
kster735 <kster735@gmail.com>
2024-07-16 16:36:54 +0300

    Added email field in registration form

--- drf/mysite/home/views.py
+++ drf/mysite/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def redirect_to_food(request):
     return render(request, 'home/redirect_to_food.html', context={})
+
+    # return redirect(request, 'food:index')
