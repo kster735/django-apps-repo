87b9d9d5ccc1abd2d41a0881e58ef9800ac60beb
kster735 <kster735@gmail.com>
2024-07-16 11:44:23 +0300

    Created users app

--- drf/mysite/mysite/settings.py
+++ drf/mysite/mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'movies.apps.MoviesConfig',
     'food.apps.FoodConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
